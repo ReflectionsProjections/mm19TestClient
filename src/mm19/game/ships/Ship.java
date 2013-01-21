d98c4e5ad481febb21458693e4c8d1499eed141f
Ace Nassri <aessa_nassri@hotmail.com>
2013-01-21 14:55:42 -0600

    Basic idea: got the class started -Added getters and setters for x,y,orientation -Added constructor -Declared a bunch of variables

--- src/mm19/game/ships/Ship.java
+++ src/mm19/game/ships/Ship.java
@@ -1,5 +1,7 @@
 package mm19.game.ships;
 
+import java.awt.geom.Point2D;
+
 /**
  * 
  * @author mm19
@@ -9,4 +11,55 @@ package mm19.game.ships;
  */
 public abstract class Ship {
 
+	// x,y passed to cstructor - cstructor sets other vars on super class
+	
+	private final int typeID; 	    // [Presumption] Type of ship
+	private int x;       			// X coordinate of ship on board
+	private int y;					// Y coordinate of ship on board
+	private final int boatLength;   // Length of the boat (boats are always of width 1)
+	private int health;			    // Health of the boat
+	private boolean isAlive = true; // Whether or not the ship is alive
+	private Orientation orientation;
+	
+	// Enum for orientation
+	public static enum Orientation { HORIZONTAL, VERTICAL }
+	
+	// Constructor
+	public Ship(int typeID, int boatLength) // x, y, orientation - g/s
+	{
+		this.typeID = typeID;
+		this.boatLength = boatLength;
+	}
+	
+	// ---- Getters ----
+	public int getX()
+	{
+		return this.x;
+	}
+	public int getY()
+	{
+		return this.y;
+	}
+	
+	public Orientation getOrientation()
+	{
+		return this.orientation;
+	}
+	
+	// ---- Setters ----
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+	
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+	
+	public void setOrientation(Orientation o)
+	{
+		this.orientation = o;
+	}
+	
 }
