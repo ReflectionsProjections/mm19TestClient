6b6c607c0749a18aa55bfb85cff8205ad76773b4
Nick Jeffrey <nickljeffrey@gmail.com>
2013-02-05 21:35:55 -0600

    Engine functions exist, but do nothing

--- src/mm19/game/Engine.java
+++ src/mm19/game/Engine.java
@@ -1,63 +1,29 @@
 package mm19.game;
 
-import mm19.game.Constants;
+import java.util.ArrayList;
+
+import mm19.game.player.Player;
 
 /**
  * @author mm19
  *
  * This will put all the pieces of the game together, and actually make things run.
  */
-public class Engine implements Runnable{
+public class Engine{
+	private Player p1;
+	private Player p2;
 
 	/**
-	 * the run function is called by the server (or API?) to start the game.
+	 * the constructor is called by the server (or API?) to start the game.
 	 */
-	@Override
-    public void run(){
-        System.out.println("And so it begins...");
+    public Engine(){
         createBoard();
-        mainLoop();
     }
 	
 	/**
-	 * createBoard will set up the variable needed to start the game, using defaults.
+	 * createBoard sets up the variables needed to start the game
 	 */
 	private void createBoard(){
-		
-	}
-	
-	/**
-	 * This function runs the actual game.
-	 * Should stop when the game ends.
-	 */
-	private void mainLoop(){
-		while(true){
-			playerTurn();
-			returnData();
-			playerTurn();
-			returnData();
-		}
-		
-	}
-	
-	/**
-	 * This function will attempt all of the player's chosen actions for the turn
-	 */
-	private void playerTurn(){
-		
-	}
-	
-	/**
-	 * This function will do the requested actions by the player
-	 */
-	private void playerAction(){
-		
-	}
-	
-	/**
-	 * This function communicates with the server (through the API), sending data about a turn to the player about to move
-	 */
-	private void returnData(){
 		
 	}
 	
@@ -65,6 +31,21 @@ public class Engine implements Runnable{
 	 * This function sets up the player's pieces on the board as specified
 	 */
 	public void playerSet(){
+		
+	}
+	
+	/**
+	 * At the start of their turn, they receive resources
+	 * This function attempts all of the player's chosen actions for the turn
+	 * Afterwards, it tells the API to send the data back
+	 */
+	private void playerTurn(){
+	}
+	
+	/**
+	 * This function does the requested actions by the player, then writes the output to the API
+	 */
+	private void playerAction(){
 		
 	}
 
