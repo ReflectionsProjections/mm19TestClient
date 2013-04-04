ca3e1287969aace8fe78e75d8258c873a30cd002
Alex Hendrix <amhendr2@illinois.edu>
2013-04-04 04:00:59 -0500

    Beginnings of a dummy client

--- src/mm19/testclient/Requester.java
+++ src/mm19/testclient/Requester.java
@@ -1,38 +1,58 @@
 package mm19.testclient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Requester {
 	Socket requestSocket;
-	ObjectOutputStream out;
-	ObjectInputStream in;
+	BufferedReader in;
+	PrintWriter out;
 	String message;
+	String playerName;
+	String token;
 	
-	Requester() {
-		
+	Requester(String playerName) {
+		this.playerName = playerName;
 	}
 	
 	void connectToServer() {
 		try {
+			byte[] b = new byte[10000];
 			requestSocket = new Socket("localhost", 6969);
 			System.out.println("Connected to server!");
-			out = new ObjectOutputStream(requestSocket.getOutputStream());
-			out.flush();
-			in = new ObjectInputStream(requestSocket.getInputStream());
 			
-			do {
-				try{ 
-					message = (String)in.readObject();
-					System.out.println(message);
-				}
-				catch(ClassNotFoundException e) {
-					e.printStackTrace();
-				}
-			} while(!message.equals("bye"));
+			
+			try{ 
+				JSONObject msg = new JSONObject();
+				msg.put("playerName", playerName);
+				String m = msg.toString();
+				
+				in = new BufferedReader(new InputStreamReader(requestSocket.getInputStream()) );
+				String s = in.readLine();
+				
+				out = new PrintWriter(requestSocket.getOutputStream());
+				out.flush();
+				System.out.println(s);
+				System.out.println(m);
+				out.println(m);
+				out.flush();
+				s = in.readLine();
+				System.out.println(s);
+					
+			}
+			catch (JSONException e) {
+				e.printStackTrace();
+			}
+
 		}
 		catch(UnknownHostException e) {
 			e.printStackTrace();
--- src/mm19/testclient/TestClient.java
+++ src/mm19/testclient/TestClient.java
@@ -2,10 +2,14 @@ package mm19.testclient;
 
 public class TestClient {
 
-		public static void run(String[] args) {
-			System.out.println("Trying to connect to server.");
-			
-			Requester r = new Requester();
-			r.connectToServer();
-		}
+	public TestClient() {
+		
+	}
+	
+	public void run() {
+		System.out.println("Trying to connect to server.");
+		
+		Requester r = new Requester("Team Pwnage");
+		r.connectToServer();
+	}
 }
