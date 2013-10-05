afb868ed85ecc74cd7fcae664687e83a7388370a
Sam Laane <laane2@acm.uiuc.edu>
2013-10-05 18:17:53 -0500

    only one at a time with name

--- src/mm19/runner/TestClientRunner.java
+++ src/mm19/runner/TestClientRunner.java
@@ -4,17 +4,20 @@ import mm19.testclient.TestClient;
 import mm19.testclient.TestClientException;
 import mm19.testclient.dummy.TestClientDummy;
 
-
 public class TestClientRunner {
-	public static void main(String args[]) {
-		TestClient tc1 = new TestClientDummy("SuperPwnageFest");
-		TestClient tc2 = new TestClientDummy("Str4y3dF4t4l1tY");
-		
-		try {
-			tc1.connect();
-			tc2.connect();
-		} catch (TestClientException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String args[]) {
+        String name = "Give_me_a_name!";
+        if(args.length >= 1) {
+            name = args[0];
+            TestClient tc1 = new TestClientDummy(name);
+
+            try {
+                tc1.connect();
+
+            }
+            catch(TestClientException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
